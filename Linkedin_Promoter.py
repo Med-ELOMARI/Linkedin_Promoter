8457ef8772716ffb8283797385d48c76d2b045ed
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2018-10-20 19:36:13 +0100

    Fixed Some Bugs

--- Linkedin_Promoter.py
+++ Linkedin_Promoter.py
@@ -8,28 +8,20 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 
-
 """
 Global parameters :
 """
-logo = """                            _                      
-|   o __  |  _  _| o __    |_) __ _ __  _ _|_ _  __
-|__ | | | |<(/_(_| | | |   |   | (_)|||(_) |_(/_ | 
-"""
-cookie_file = "your_cookies.pkl"
 MAX_CLICK_TRIES = 3
-
 
 """
 Parce user Data from the args 
 """
-parser = argparse.ArgumentParser(add_help=True)
+parser = argparse.ArgumentParser(add_help=True, description='Promote your linkedin Account by enlarging your network ')
 parser.add_argument('-e', type=str, help="your linkedin email .")
 parser.add_argument('-p', type=str, help="your linkedin password .")
 args = parser.parse_args()
 
-
-# TODO : Add those args to argparser
+# TODO : Add those args to argparser and fix deafult command sets
 """
 You can play around with these arguments like making it headless or changing loging level
 it's on your court now ;)
@@ -49,6 +41,7 @@ print "[+] Starting Browser ...",
 driver = webdriver.Chrome('ChromeDriver', chrome_options=chrome_options)
 print " OK"
 
+
 def click(by, what_to_click):
     """
     this function Click on elements "what_to_click" by the "by" argument
@@ -56,7 +49,6 @@ def click(by, what_to_click):
 
     :param by: how to search ... by class or xpath ...
     :param what_to_click: what to element you want to click
-
     """
     clicked = False
     tries = 0
@@ -71,7 +63,7 @@ def click(by, what_to_click):
             clicked = True
         except:
             tries += 1
-            print "[+] Can't click on ", what_to_click, " ! (" + str(tries) + "/" + str(MAX_CLICK_TRIES) + ") Trying Again !"
+            print "[+] Can't Add  ! (" + str(tries) + "/" + str(MAX_CLICK_TRIES) + ") Trying Again !"
             sleep(1)
 
 
@@ -137,7 +129,7 @@ def connect_by_keywords(keywords, max):
     :return:
     """
     for word in keywords:
-        print "[+] Working On Keyword :",word," ..."
+        print "[+] Working On Keyword :", word, " ..."
         added = 0
         for i in xrange(1, (max / 10) + 1):
             driver.get("https://www.linkedin.com/search/results/all/?keywords=" + word + "&page=" + str(i))
@@ -146,7 +138,7 @@ def connect_by_keywords(keywords, max):
                 # print "clicking :", Connect.text
                 driver.execute_script("arguments[0].click();", Connect)
                 added += 1
-                print "[+] ",added," Added !"
+                print "[+] ", added, " Added !"
                 # Connect.click()
                 try:
                     click("class", "button-primary-large")
@@ -195,33 +187,42 @@ def Confirm():
     """
     m.getch()
 
+
 def menu():
     """
     Main menu for choosing witch task to do from the 3 modes
     :return:
     """
+    logo = """                            _                      
+    |   o __  |  _  _| o __    |_) __ _ __  _ _|_ _  __
+    |__ | | | |<(/_(_| | | |   |   | (_)|||(_) |_(/_ | 
+    """
+
     while True:
         print "=" * 80
         print logo
         print " " * 9, "Linkedin Promoter - By Venus-Dev"
         print "=" * 80
         print " " * 10, "[1] - Add from section People you may know"
-        print " " * 10, "[2] - Add By keywords "
+        print " " * 10, "[2] - Add By keywords and Topics "
         print " " * 10, "[3] - Withdraw All Sent Connections"
         print "=" * 80
-        choice = input("[+] Your Choice :")
-        if choice in [1, 2, 3]:
-            break
+        try:
+            choice = input("[+] Your Choice :")
+            if choice in [1, 2, 3]:
+                break
+        except SyntaxError:
+            pass
 
     if choice == 1:
         print "=" * 80
-        driver.get("https://www.linkedin.com/mynetwork/")
         max_accounts = input("[+] Enter Max accounts to scrap (ex :100):")
+        driver.get("https://www.linkedin.com/mynetwork/")
         connect_to_my_network(scrap_accounts(max_accounts))
 
     elif choice == 2:
         print "=" * 80
-        keywords = raw_input("[+] Please Enter your Keywords separated by space :").split()
+        keywords = raw_input("[+] Please Enter your Keywords separated by '+' :").split("+")
         max_accounts = input("[+] Enter Max accounts to scrap (ex :100):")
         connect_by_keywords(keywords, max_accounts)
 
@@ -233,6 +234,7 @@ def menu():
         print "[!] Press Enter to Continue"
         Confirm()
         withdraw_all()
+
 
 def main():
     """
@@ -247,6 +249,8 @@ def main():
     except KeyboardInterrupt:
         print  "[-] Good Bye"
         exit(0)
+    except WebDriverException:
+        print "Selenuim Error ! ", sys.exc_info()
 
 
 if __name__ == '__main__':
